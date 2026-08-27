959cf27dd77cda8830482810b6e3ef2483fc61d7
Géraldy <geraldymakosso@gmail.com>
2026-08-27 19:26:27 +0100

    fix: declare smooth scroll behavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   };
 
   return (
-    <html lang="fr" className={inter.variable}>
+    <html lang="fr" className={inter.variable} data-scroll-behavior="smooth">
       <body>
         <SiteHeader />
         <main>{children}</main>
